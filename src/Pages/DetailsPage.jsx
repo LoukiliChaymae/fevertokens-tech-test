c7aa258b8d575a6eaab0cef8199a79ac70673096
chaymae <c.loukili1999@gmail.com>
2023-11-06 20:28:14 +0100

    working on details

--- src/Pages/DetailsPage.jsx
+++ src/Pages/DetailsPage.jsx
@@ -1,6 +1,7 @@
 import React,{useEffect,useState} from 'react'
 import { useParams } from 'react-router-dom/dist';
 import HeaderSection from '../components/HeaderSection';
+import Chart from '../components/Chart';
 
 function DetailsPage() {
     const { Id } = useParams();
@@ -22,40 +23,31 @@ function DetailsPage() {
         fetchData();
     }, [Id]);
 
-    useEffect(() => {
-      const fetchForChart = async()=>{
-        try {
-        const fetchChart= await fetch(`https://api.coingecko.com/api/v3/coins/${Id}/market_chart?vs_currency=usd&days=30&interval=daily`)
-        const chart = await fetchChart.json();
-        console.log("chart",chart)
-        setChartInfo(chart);
-      }  catch (error) {
-        console.error("Une erreur s'est produite lors de la récupération des données :", error);
-    }}
-      fetchForChart()
-    }, [])
+    
     
   return (   
-    <div className="h-screen bg-gradient-to-r from-[#0C0A1D] to-[#17162B]"> <div className=" flex-col "><HeaderSection className="mt-4 flex justify-center items-center" />
+    <div className="h-screen bg-gradient-to-r from-[#0C0A1D] to-[#17162B]"> <HeaderSection className="mt-4 flex justify-center items-center" />
 
-    <div className="flex flex-1 ">
-    <div className="w-2/5 bg-gray-300 flex justify-center">
-      <div className='lg:w-[98%] rounded overflow-hidden shadow-lg bg-blue-500'>
-        <div class="rounded-md justify-center text-sm bg-zinc-950 text-white flex  w-[4rem] px-0">Rank #{a}</div>
-        <div className="flex gap-2 items-center" >
+    <div className="flex">
+    <div className="w-2/5  flex justify-center">
+      <div className='lg:w-[98%] rounded overflow-hidden shadow-lg bg-white p-6'>
+        <div className="rounded-md justify-center text-sm bg-zinc-950 text-white flex w-[4rem] px-0">Rank #{a}</div>
+        <div className="flex gap-2 items-center h-[50px]" >
           <div><img width={'25px'} height={'25px'} src={details?.image?.small}/></div>
-            <div className="font-bold text-xl">{details.name}</div>
+            <div className="font-bold text-x1">{details.name}</div>
             <div className="font-light text-base">{details.symbol}</div>
         </div>
-        <div>{details?.market_data?.current_price?.usd}</div>
-        <div></div>
+        <div className='font-bold text-lg'>${details?.market_data?.current_price?.usd}</div>
+        <progress className="rounded-full tw-flex h-[8px] tw-justify-between tw-w-full high-low-range-slider tw-mt-3" min={0} max={details?.market_data?.high_24h?.usd - details?.market_data?.low_24h?.usd} value={details?.market_data?.current_price?.usd - details?.market_data?.low_24h?.usd}></progress>
       </div>
     </div>
-    <div class="w-3/5 bg-gray-500 flex justify-center"> 
-     <div className='lg:w-[98%] rounded overflow-hidden shadow-lg bg-red-500'>chart</div>
+    <div className="w-3/5  flex justify-center"> 
+     <div className='lg:w-[98%] rounded overflow-hidden shadow-lg '>
+     <Chart/>
+     </div>
     </div>
-    
-    </div></div></div>  
+      
+    </div></div> 
   )
 }
 
---
+++ src/components/Chart.jsx
@@ -0,0 +1,91 @@
+// Data generated from http://www.bikeforums.net/professional-cycling-fans/1113087-2017-tour-de-france-gpx-tcx-files.html
+// import Highcharts from 'highcharts';
+import { useEffect ,useState, React} from 'react';
+import { useParams } from 'react-router-dom/dist';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import moment from 'moment';
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Filler,
+    Legend
+  );
+
+export const options = {
+responsive: true,
+plugins: {
+legend: {
+  position: 'top' ,
+},
+title: {
+  display: true,
+  text: 'Chart.js Line Chart',
+},
+},
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+
+// Now create the chart
+function Chart(){
+    const { Id } = useParams();
+    const [chartInfo, setChartInfo] = useState([]);
+    useEffect(() => {
+        const fetchForChart = async()=>{
+          try {
+          const fetchChart= await fetch(`https://api.coingecko.com/api/v3/coins/${Id}/market_chart?vs_currency=usd&days=30&interval=daily`)
+          const chart = await fetchChart.json();
+          const wewe = chart.prices.map( (price) => (
+            {
+                x : price[0],
+                y : price[1].toFixed(2)
+        
+            }
+            
+        )
+        
+        )
+          setChartInfo( wewe
+        
+        );
+        console.log("chartInfo",wewe);
+        }  catch (error) {
+          console.error("Une erreur s'est produite lors de la récupération des données :", error);
+      }}
+        fetchForChart()
+      }, [])
+
+      const data = {
+        labels:chartInfo.map(value => moment(value.x). format('MMM DD')),
+        datasets: [
+        {
+          fill: true,
+          label: 'Dataset 2',
+          data: chartInfo.map((info)=> info.y),
+          borderColor: 'rgb(53, 162, 235)',
+          backgroundColor: 'rgba(53, 162, 235, 0.5)',
+        },
+        ],
+        };
+   
+    return(
+        <Line options={options} data={data} />
+      );
+}
+export default Chart;
